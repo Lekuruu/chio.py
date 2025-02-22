7859c6f2cb69b5bd1a62d88b037680aa94cff62b
Lekuru <contact@lekuru.xyz>
2025-02-22 23:59:44 +0100

    Add support for `b298`

--- chio/clients/__init__.py
+++ chio/clients/__init__.py
@@ -4,12 +4,14 @@ from .b282 import b282
 from .b291 import b291
 from .b294 import b294
 from .b296 import b296
+from .b298 import b298
 
 ClientDict = {
     282: b282(), 290: b282(),
     291: b291(), 293: b291(),
     294: b294(), 295: b294(),
-    296: b296(), 297: b296()
+    296: b296(), 297: b296(),
+    298: b298(), 311: b298(),
 }
 
 HighestVersion = max(ClientDict.keys())
