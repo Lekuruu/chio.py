4d0c64cb9a4f6ce39598e487f9db55475df79ceb
Lekuru <contact@lekuru.xyz>
2025-02-25 19:44:53 +0100

    Add missing `ReceiveUpdates` packet

--- chio/clients/b634.py
+++ chio/clients/b634.py
@@ -7,10 +7,14 @@ from ..io import *
 
 class b634(b613):
     """
-    b634 adds the monitor packet.
+    b634 adds the monitor & player update request packets.
     """
     version = 634
 
     @classmethod
     def write_monitor(cls) -> Iterable[Tuple[PacketType, bytes]]:
         yield PacketType.BanchoMonitor, b''
+
+    @classmethod
+    def read_receive_updates(cls, stream: MemoryStream) -> PresenceFilter:
+        return PresenceFilter(read_s32(stream))
