90b15486eea49b73ab51b55e3f5fdacd4fde0bcb
Lekuru <contact@lekuru.xyz>
2025-04-18 16:32:29 +0200

    Add `resolve_country_index` function

--- chio/utils.py
+++ chio/utils.py
@@ -1,6 +1,17 @@
 
 from .clients import ClientDict, LowestVersion, HighestVersion
+from .constants import CountryAcronyms
 from .chio import BanchoIO
+
+def resolve_country_index(country_acronym: str) -> int:
+    """
+    Resolve the country index from the acronym.
+    If the acronym is not found, it will return 0.
+    """
+    return (
+        CountryAcronyms.index(country_acronym)
+        if country_acronym in CountryAcronyms else 0
+    )
 
 def select_client(version: int) -> BanchoIO:
     """Select the appropriate client based on the version provided."""
