d0c0db5e5f1b3fe5d672fb189401ea4a049da8eb
Lekuru <contact@lekuru.xyz>
2025-04-19 18:34:21 +0200

    Add util functions/properties to mode enum

--- chio/constants.py
+++ chio/constants.py
@@ -187,6 +187,36 @@ class Mode(IntEnum):
     CatchTheBeat = 2
     OsuMania     = 3
 
+    @classmethod
+    def from_alias(cls, input: str):
+        if input not in ('osu', 'taiko', 'fruits', 'mania'):
+            return
+
+        return {
+            'osu': Mode.Osu,
+            'taiko': Mode.Taiko,
+            'fruits': Mode.CatchTheBeat,
+            'mania': Mode.OsuMania
+        }[input]
+
+    @property
+    def formatted(self) -> str:
+        return {
+            Mode.Osu: 'osu!',
+            Mode.Taiko: 'Taiko',
+            Mode.CatchTheBeat: 'CatchTheBeat',
+            Mode.OsuMania: 'osu!mania'
+        }[self]
+
+    @property
+    def alias(self) -> str:
+        return {
+            Mode.Osu: 'osu',
+            Mode.Taiko: 'taiko',
+            Mode.CatchTheBeat: 'fruits',
+            Mode.OsuMania: 'mania'
+        }[self]
+
 class LoginError(IntEnum):
     InvalidLogin          = -1
     InvalidVersion        = -2
