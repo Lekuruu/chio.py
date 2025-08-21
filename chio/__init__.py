85d273bc20df2c90008fd37250e537ad915ec605
Lekuru <contact@lekuru.xyz>
2025-08-21 20:08:09 +0200

    Remove reference to `format_chat_message_to_markdown`

--- chio/__init__.py
+++ chio/__init__.py
@@ -4,7 +4,7 @@ __email__ = "contact@lekuru.xyz"
 __version__ = "1.1.20"
 __license__ = "MIT"
 
-from .utils import select_client, select_latest_client, select_initial_client, resolve_country_index, format_chat_message_to_markdown
+from .utils import select_client, select_latest_client, select_initial_client, resolve_country_index
 from .patching import patch, set_protocol_version, set_slot_size
 from .chio import BanchoIO
 from .io import Stream
